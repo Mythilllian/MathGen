beac4d0e73ed9b356f8da3a24d23abc5fccc710c
Leo G <Leo G@DESKTOP-LEOG>
2023-09-23 08:52:16 -0400

    Add project files.

---
+++ MathProblemGenerator/Variable.cs
@@ -0,0 +1,23 @@
+﻿namespace MathProblemGenerator
+{
+    public struct Variable
+    {
+        //Minimum should be <= maximum
+        public float minVal { get { return minVal; } set { minVal = value <= maxVal ? value : minVal; } }
+
+        //Maximum should be >= minimum
+        public float maxVal { get { return maxVal; } set { maxVal = minVal <= value ? value : maxVal; } }
+
+        //Precision is what value is multiple of
+        public int precision = 1;
+
+        public char character;
+
+        public Variable(float minVal, float maxVal, int precision, char character)
+        {
+            this.minVal = minVal;
+            this.maxVal = maxVal;
+            this.precision = precision
+        }
+    }
+}
