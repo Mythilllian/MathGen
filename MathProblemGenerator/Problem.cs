7edccff7d801bca144f26bd57cc45fdb0e855867
Mythilllian <28gorell@gmail.com>
2023-09-23 11:16:42 -0400

    (Still in progress) - Generates values of variables on declaration - Random variable for generating random values for the variables - JsonPropertyNames - Commented format of problems and answers

---
+++ MathProblemGenerator/Problem.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MathProblemGenerator
+{
+    public class Problem
+    {
+        [JsonPropertyName("vars")]
+        List<Variable> variables;
+
+        [JsonPropertyName("txt")]
+        string problemText;
+
+        [JsonPropertyName("ans")]
+        double answer;
+
+        [JsonIgnore]
+        Random rand;
+
+        public Problem(List<Variable> variables, string problemText, double answer)
+        {
+            this.variables = variables;
+            this.problemText = problemText;
+            this.answer = answer;
+
+            rand = new Random();
+
+            foreach(var variable in variables)
+            {
+                variable.getVal(rand);
+            }
+        }
+    }
+}
