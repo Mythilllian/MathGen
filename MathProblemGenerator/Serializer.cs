fe8c1929965d9f3a8f6d59d805be57c01fa75eb3
Mythilllian <28gorell@gmail.com>
2023-09-23 11:27:47 -0400

    Serializes and loads Json files, folders, and text

---
+++ MathProblemGenerator/Serializer.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+
+namespace MathProblemGenerator
+{
+    public static class Serializer
+    {
+        public static Problem? LoadProblemFromJson(string json)
+        {
+            try
+            {
+                return (Problem?)JsonSerializer.Deserialize(json, typeof(Problem));
+            }
+            catch { return null; }
+        }
+
+        public static Problem? LoadProblemFromPath(string path)
+        {
+            if(File.Exists(path) && Path.GetExtension(path) == ".json")
+            {
+                return LoadProblemFromJson(File.ReadAllText(path));
+            }
+            return null;
+        }
+
+        public static Problem[] LoadProblemFolder(string path)
+        {
+            List<Problem> problems = new List<Problem>();
+
+            foreach(var file in Directory.GetFiles(path, "*.json"))
+            {
+                try
+                {
+                    problems.Add(LoadProblemFromPath(file));
+                }
+                catch { }
+            }
+
+            return problems.ToArray();
+        }
+
+        public static void SaveProblem(Problem problem)
+        {
+            JsonSerializer.Serialize(problem);
+        }
+    }
+}
