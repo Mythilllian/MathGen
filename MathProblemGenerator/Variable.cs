2518af304ab48b42f695d94f326bf0cef39f6f31
Mythilllian <28gorell@gmail.com>
2023-09-23 11:12:41 -0400

    - Precision->factor - Values have to be divisible by factor - Changing factor automatically adjusts values - Added function that randomly generates the value in betwen min and max - Add JsonPropertyNames

--- MathProblemGenerator/Variable.cs
+++ MathProblemGenerator/Variable.cs
@@ -1,23 +1,44 @@
-﻿namespace MathProblemGenerator
+﻿using System.Text.Json.Serialization;
+
+namespace MathProblemGenerator
 {
     public struct Variable
     {
         //Minimum should be <= maximum
-        public float minVal { get { return minVal; } set { minVal = value <= maxVal ? value : minVal; } }
+        [JsonPropertyName("min")]
+        public double minVal { get { return minVal; } set { minVal = value <= maxVal && value % factor == 0 ? value : minVal; } }
 
         //Maximum should be >= minimum
-        public float maxVal { get { return maxVal; } set { maxVal = minVal <= value ? value : maxVal; } }
+        [JsonPropertyName("max")]
+        public double maxVal { get { return maxVal; } set { maxVal = minVal <= value && value % factor == 0 ? value : maxVal; } }
 
-        //Precision is what value is multiple of
-        public int precision = 1;
+        //factor is what value is multiple of
+        [JsonPropertyName("fac")]
+        public double factor { get { return factor; } set {
+                minVal = Math.Round((minVal / (double)value), MidpointRounding.AwayFromZero) * value;
+                maxVal = Math.Round((maxVal / (double)value), MidpointRounding.AwayFromZero) * value;
+                factor = value;
+            } }
 
-        public char character;
+        [JsonPropertyName("char")]
+        public char character { get; set; }
 
-        public Variable(float minVal, float maxVal, int precision, char character)
+        [JsonIgnore]
+        public double val { get; private set; }
+
+        public Variable(float _minVal, float _maxVal, int _factor, char _character) : this()
         {
-            this.minVal = minVal;
-            this.maxVal = maxVal;
-            this.precision = precision
+            minVal = _minVal;
+            maxVal = _maxVal;
+            factor = _factor;
+            character = _character;
+        }
+
+        public double getVal(Random rand)
+        {
+            val = rand.Next((int)(minVal / factor), (int)(maxVal / factor + 1)) * factor;
+
+            return val;
         }
     }
 }
